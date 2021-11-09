2f9d6d3512b887cd56cc17dc8a1d1751c22312ed
OmarO <omar_orejuela@hotmail.com>
2021-11-09 08:59:18 -0500

    Actualizacion con la clase

--- FrontEnd/turisapp/src/app/components/inicio/inicio.component.ts
+++ FrontEnd/turisapp/src/app/components/inicio/inicio.component.ts
@@ -8,12 +8,34 @@ const URL_SITIO='GOOGLE.COM';
 export class InicioComponent implements OnInit {
   isAct:boolean = true;
   edad:number=15;
-  constructor() { }
+  anionac:number=2007;
+  nombre='Camilo Perez';
+  mensaje:string='';
+  edades:number[]=[16, 22, 35, 46];
+  heroes=[
+    {nombre:'SpiderMan',
+    personaje:'Peter',
+    descripcion:'Este es el hombre araña',
+    foto:'assets/fotos/SpiderMan.jfif'},
+
+    {nombre:'IronMan',
+    personaje:'Luke',
+    descripcion:'Este es el hombre de hierro',
+    foto:'assets/fotos/IronMan.jpg'},
+
+    {nombre:'Hulk',
+    personaje:'Bruce',
+    descripcion:'Este es el hombre increible',
+    foto:'assets/fotos/Hulk.jpg'}
+  ]
+  constructor() {
+    this,this.mensaje=`Hola me llamo ${this.nombre} y tengo ${2021-this.anionac} años de edad, y nací en Colombia`;
+   }
 
   ngOnInit(): void {
     console.log(this.isAct);
     if(this.isAct){
-      console.log(this.edad);
+      console.log(this.mensaje);
     }
   }
 
