bc53008b6bc48bf48a54d1e03383266c6f947e04
OmarO <omar_orejuela@hotmail.com>
2021-11-11 10:58:29 -0500

    actualizacion

--- FrontEnd/turisapp/src/app/app-routing.module.ts
+++ FrontEnd/turisapp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from 'Grupo1-Ciclo4/FrontEnd/turisapp/src/app/components/inicio/inicio.component';
-import { LoginComponent } from 'Grupo1-Ciclo4/FrontEnd/turisapp/src/app/components/inicio/inicio.component'
+import { InicioComponent } from 'C:/Users/Personal/Documents/GitHub/Grupo1-Ciclo4/FrontEnd/turisapp/src/app/components/inicio/inicio.component';
+import { LoginComponent } from 'C:/Users/Personal/Documents/GitHub/Grupo1-Ciclo4/FrontEnd/turisapp/src/app/components/login/login.component';
 
 const routes: Routes = [
   {path: 'autenticacion', component:LoginComponent},
